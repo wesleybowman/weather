002637bbf2d5e28b3f1d8632f35689bd51b9a461
evgorchakov@gmail.com <evgorchakov@gmail.com>
2013-04-20 15:25:18 -0400

    ?

--- README.md
+++ README.md
@@ -11,10 +11,15 @@ Maximum 20 requests/hour due to API limitations.
 2. get location by external IP
 3. get weather data by location
 
+
 **What it uses**:
 * [requests](http://docs.python-requests.org/) module
 * [OpenWeatherMap JSON API](http://openweathermap.org/wiki/API/JSON_API)
 * [geobytes IP locator](http://www.geobytes.com/IpLocator.htm?GetLocation)
+
+**Requirements:
+* python3
+* [requests](http://docs.python-requests.org/) module
 
 **To Do**:
 - improve time 
@@ -29,13 +34,13 @@ Maximum 20 requests/hour due to API limitations.
 
 **Sample output**:
 	
-        [evg@arch weather]$ ./weather.py 
+[evg@arch weather]$ ./weather.py 
 
-                Ottawa, CA
-                Updated:        2013-04-20 01:40 GMT
-                Time now:       2013-04-20 02:22 GMT
-                ------------------------------------
-                Temperature:    9  C
-                Condition:      overcast clouds
-                Wind:           2.57 m/s
-                Humidity:       83 %
+        Ottawa, CA
+        Updated:        2013-04-20 18:42:31 GMT
+        Time now:       2013-04-20 19:17:40 GMT
+        ---------------------------------------
+        Temperature:    4 (C)
+        Wind:           9.8 (m/s)
+        Humidity:       48 (%)
+        Condition:      overcast clouds
--- weather.py
+++ weather.py
@@ -3,7 +3,9 @@
 
 import json
 import requests
+from functools import reduce
 from time import gmtime, strftime
+
 
 # URLs for retrieving IP, locations, weather data
 
@@ -12,50 +14,67 @@ LOCATION_URL = "http://www.geobytes.com/IpLocator.htm?GetLocation"
 WEATHER_URL = "http://api.openweathermap.org/data/2.1/find/name"
 
 
+def safe_get(*args):
+	try:
+		return reduce(lambda a,b: a[b], args)
+	except KeyError:
+		return 'N/A'
+
 def get_IP():
 	return requests.get(IP_URL).text
 
 def get_location(ip):
 	location_params = {'template':'json.txt', 'IpAddress':ip}
 	location_json = requests.get(LOCATION_URL, params=location_params).json()
-	country = location_json['geobytes']['country']
-	city = location_json['geobytes']['city']
+	country = safe_get(location_json,'geobytes','country')
+	city = safe_get(location_json,'geobytes','city')
 	return (city, country)
 
 def get_weather_data(city, country, units='metric'):
 	weather_params = {'q':'{0},{1}'.format(city,country), 'units': units}
 	weather_json = requests.get(WEATHER_URL, params=weather_params).json()
-	return weather_json
 
-def output_weather(weather):
-	w=weather['list'][0]  #temp var to decrease number of chars used
-	city = w['name']
-	country = w['sys']['country']
-	date = w['date'][:-3]
-	temp = round(w['main']['temp'])
-	desc = w['weather'][0]['description']
-	windspeed = w['wind']['speed']
-	humidity = w['main']['humidity']
-	current_time = strftime("%Y-%m-%d %H:%M",gmtime())
+	w = safe_get(weather_json,'list',0)  #temp var 
+	
+	weather_data = {
+		'city' : 		safe_get(w,'name'),
+		'country' : 	safe_get(w,'sys','country'),
+		'date' : 		safe_get(w,'date'),
+		'temp' :		safe_get(w,'main','temp'), 
+		'description' :	safe_get(w,'weather',0,'description'),
+		'windspeed' : 	safe_get(w,'wind','speed'),
+		'humidity' : 	safe_get(w,'main','humidity')
+	}
+
+	try:
+		weather_data['temp']=round(weather_data['temp'])
+	except TypeError:
+		pass
+
+	return weather_data
+
+def output_weather(weather_data):
+	current_time = strftime("%Y-%m-%d %H:%M:%S",gmtime())
+
 	print(
 	"""
 	{0}
 	Updated:	{1} GMT
 	Time now:	{2} GMT
-	------------------------------------
-	Temperature:	{3}  C
+	---------------------------------------
+	Temperature:	{3} (C)
+	Wind:		{5} (m/s)
+	Humidity:	{6} (%)
 	Condition:	{4}
-	Wind:		{5} m/s
-	Humidity:	{6} %
 	"""
 	.format(
-		city+', '+country,
-		date,
+		weather_data['city']+', '+weather_data['country'],
+		weather_data['date'],
 		current_time,
-		temp,
-		desc,
-		windspeed,
-		humidity		
+		weather_data['temp'],
+		weather_data['description'],
+		weather_data['windspeed'],
+		weather_data['humidity']		
 		)
 	)
 
